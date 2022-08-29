46edd8f74b114820c7d2a50c2d0890d77c15ad06
Carole Nguyen <mlcarole.nguyen@gmail.com>
2022-08-29 16:39:11 +0200

    routes OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get 'dashboards', to: 'pages#dashboard'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :missions, only: [:index, :show] do
+    resources :bookings, only: [:show, :create, :update, :edit]
+  end
+  resources :bookings, only: [:destroy]
 end
