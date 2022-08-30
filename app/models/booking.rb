60d694272cc388d063cc5f8e780be72c1b954530
clara basuyau <clarabasuyau@gmail.com>
2022-08-30 15:00:44 +0200

    generate review model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,8 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :mission
 
+  has_many :reviews, dependent: :destroy
+
   validates :start_date, :end_date, presence: true
   # enum status: { pending: 0, accepted: 1, declined: 2 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_141426) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_125914) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_141426) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.bigint "booking_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+  end
+
   create_table "tags", force: :cascade do |t|
     t.string "name"
     t.bigint "category_id", null: false
@@ -81,6 +89,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_141426) do
   add_foreign_key "bookings", "users"
   add_foreign_key "mission_tags", "missions"
   add_foreign_key "mission_tags", "tags"
+  add_foreign_key "reviews", "bookings"
   add_foreign_key "tags", "categories"
   add_foreign_key "users", "categories"
 end
