6f19597b1aa1b140b17c1eaf83d10ff8c6461af5
Kevin Trinh <kxt.trinh@gmail.com>
2022-08-30 14:15:20 +0200

    just test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "open-uri"
+require "nokogiri"
+
+img = []
+i = 0
+
+html = URI.open("https://www.doublesens.fr/26-action-solidaire").read
+# 1. Parse HTML
+doc = Nokogiri::HTML(html, nil, "utf-8")
+doc.search(".product-min-container img").each_with_index do |ele, index|
+  img << ele.attr("src") if index.even? && index <= 28
+  img << ele.attr("src") if index.odd? && index >= 28
+end
+
+
+puts img
+puts img.size
