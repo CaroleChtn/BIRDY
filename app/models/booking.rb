68a4f4fb620b888554c9734a5169270d81538636
clara basuyau <clarabasuyau@gmail.com>
2022-08-30 12:18:15 +0200

    add actions to booking controller

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :mission
+
+  validates :start_date, :end_date, presence: true
+  # enum status: { pending: 0, accepted: 1, declined: 2 }
 end
