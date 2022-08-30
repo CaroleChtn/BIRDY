5776de13dc7a09713dbf6e8880f6e5627ea23d3d
Gevorgyan Ruzanna <ruz.gevor@gmail.com>
2022-08-30 12:54:51 +0200

    scrapping card action solidaire v1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
+require "nokogiri"
+
+img = []
+city = []
+title = []
+departure_date = [] #sur certaines missions y'a pas de date départ prévu, du coup sur la vue -> faire une conidition "if "nil" indiquer "pas de départ prévu"
+price = []
+duration= []
+
+html = URI.open("https://www.doublesens.fr/26-action-solidaire").read
+doc = Nokogiri::HTML(html, nil, "utf-8")
+doc.search(".product-min-container img").each_with_index do |element, index|
+  img << element.attr("src") if index.even? && index <= 28
+  img << element.attr("src") if index.odd? && index >= 28
+end
+
+
+doc.search(".product-link").each_with_index do |element, index|
+  city << element.text if index.even?
+  title << element.text if index.odd?
+end
+
+
+doc.search(".product-miniature-depart-container").each do |element|
+  departure_date << element.text.strip
+end
+
+
+doc.search(".price").each do |element|
+  price << element.text.strip
+end
+
+
+doc.search(".nbjours").each do |element|
+  duration << element.text.strip
+end
