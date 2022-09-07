28a83f85c013aa08e66c0c6e88713939d1bc8d07
Gevorgyan Ruzanna <ruz.gevor@gmail.com>
2022-09-07 16:57:40 +0200

    favori navbar rouge

--- app/javascript/controllers/favorites_controller.js
+++ app/javascript/controllers/favorites_controller.js
@@ -17,13 +17,18 @@ export default class extends Controller {
       .then(response => response.json())
       .then((data) => {
         if (data.unfav) {
+          console.log(1)
           if (data.page === "/dashboards") {
             evt.target.closest('.card-index').remove();
           } else {
+            console.log(2)
             evt.target.style.color = "black";
           }
         } else {
+          console.log(3)
           evt.target.style.color = "red";
+          const heart = document.querySelector('#top-heart')
+          heart.style.color = 'red';
         }
       })
     }
