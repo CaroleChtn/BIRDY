a151c6e0e6528106f62114a5e805a3d26ad248c1
Nidal Moughazi <nid.pro95@gmail.com>
2022-09-08 12:32:27 +0200

    empreinte

---
+++ app/javascript/controllers/toggle_empreinte.js
@@ -0,0 +1,24 @@
+// import { Controller } from "@hotwired/stimulus"
+
+// // Connects to data-controller="navbar"
+// export default class extends Controller {
+// static targets = ["item", "short"]
+
+//   connect() {
+//  console.log("test")
+//   }
+
+//   toggle(event) {
+//     console.log("click")
+//     this.itemTarget.classList.toggle("d-none")
+//     this.shortTarget.classList.toggle("d-none")
+//       $('#panel,#panel-2').sidr({
+//         side: 'right',
+//         displace: false
+//     });
+//     $('#panel,#panel-2').click(function(){
+//         $('#panel-2').toggle();
+//         $('#panel').toggle();
+//     });
+//   }
+// }
